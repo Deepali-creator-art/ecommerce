4ec775975d0cc3600fdebad4c7b9f4e7a6ae6dcd
Deepali-creator-art <134923246+Deepali-creator-art@users.noreply.github.com>
2024-11-23 14:07:47 +0530

    Update models.py

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -27,5 +27,57 @@ class Product(models.Model):
         return reverse('products_detail',args=[self.category.slug,self.slug]) 
     def __str__(self):
         return self.name 
-       
+class Cart(models.Model):
+    cart_id=models.CharField(max_length=250,blank=True)
+    date_added=models.DateField(auto_now_add=True)
+    class Meta:
+        db_table='Cart'
+        ordering=['date_added']
+    def __str__(self):
+        return self.cart_id
+class CartItem(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    cart=models.ForeignKey(Cart,on_delete=models.CASCADE)
+    quantity=models.IntegerField()
+    active=models.BooleanField(default=True)
+    class Meta:
+        db_table='CartItem'
+    def sub_total(self):
+        return self.product.price*self.quantity
+    def __strt__(self):
+        return self.product
+class Order(models.Model):
+    token=models.CharField(max_length=250,blank=True)
+    total=models.DecimalField(max_digits=10,decimal_places=2,verbose_name='Indian Order Total')
+    emailaddress=models.EmailField(max_length=250,blank=True,verbose_name='Email Address')
+    created=models.DateTimeField(auto_now_add=True)
+    billingName=models.CharField(max_length=250,blank=True)
+    billingAddress1=models.CharField(max_length=250,blank=True)
+    billingCity=models.CharField(max_length=250,blank=True)
+    billingPostcode=models.CharField(max_length=250,blank=True)
+    billingCountry=models.CharField(max_length=250,blank=True)
+    shippingName=models.CharField(max_length=250,blank=True)
+    shippingAddress1=models.CharField(max_length=250,blank=True)
+    shippingCity=models.CharField(max_length=250,blank=True)
+    shippingPostcode=models.CharField(max_length=250,blank=True)
+    shippingCountry=models.CharField(max_length=250,blank=True)
+    class Meta:
+        db_table='Order'
+        ordering=['-created']
+    def __str__(self):
+        return str(self.id)
+class OrderItem(models.Model):
+    product=models.CharField(max_length=250)
+    quantity=models.IntegerField()
+    price=models.DecimalField(max_digits=10,decimal_places=2,verbose_name='Indian Price')
+    order=models.ForeignKey(Order,on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table='OrderItem'
+    def sub_total(self):
+        return self.quantity*self.price 
+    def __str__(self):
+        return self.product   
+                                 
+           
       
