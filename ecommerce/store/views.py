76a06a77782bec4a1088f5ca9aec4336fd671413
Deepali-creator-art <134923246+Deepali-creator-art@users.noreply.github.com>
2024-11-24 13:47:50 +0530

    Update views.py

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate,login,logout
 from .models import *
 from django.core.exceptions import ObjectDoesNotExist
-
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib import messages
 def home(request,category_slug=None):
     category_page=None
     products=None
