486ae588c43b743ad2ffc81879e9eed634f8547d
Deepali-creator-art <134923246+Deepali-creator-art@users.noreply.github.com>
2024-11-24 13:44:55 +0530

    Update views.py

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -51,6 +51,21 @@ def SignIn(request):
         'form':form
     }
     return render(request,'signin.html',context)
+def activate(request,uidb64,token):
+    #activate  the user by settings the is_active status to True
+    try:
+        uid=urlsafe_base64_decode(uidb64).decode()
+        user=User._default_manager.get(pk=uid)
+    except(TypeError,ValueError,OverflowError,User.DoesNotExist):
+        user=None
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active=True
+        user.save()
+        messages.success(request,"Congratulations Your account is activated")
+        return redirect('home')
+    else:
+        messages.error(request,"Invalid activation link")
+        return redirect('signin')
 def LogOut(request):
     logout(request)
     return redirect('signin')
