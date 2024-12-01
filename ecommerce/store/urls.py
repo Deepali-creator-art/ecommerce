11d0687a6dcc9ee2b9cd2954b68f70bad37ec236
Deepali-creator-art <134923246+Deepali-creator-art@users.noreply.github.com>
2024-12-01 14:43:51 +0530

    Update urls.py

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
-from django.contrib.auth.views import PasswordChangeView,PasswordChangeDoneView
+
 from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView,PasswordChangeDoneView,PasswordResetView,PasswordResetConfirmView,PasswordResetDoneView,PasswordResetCompleteView
+urlpatterns = [
 urlpatterns = [
     path('',home,name='home'),
     path("category/<slug:category_slug>",home,name='products_by_category'),
@@ -15,9 +17,15 @@ urlpatterns = [
     path('signup/',SignUp,name='signup'),
     path('signin/',SignIn,name='signin'),
     path('logout/',LogOut,name='logout'),
+    path('search/',search,name='search'),
+    path('activate/<uidb64>/<token>/',activate,name='activate'),
     #Set up url for password change 
     path("change_password/",PasswordChangeView.as_view(template_name='password_operations/password_change_form.html',form_class = PasswordChangeForm, success_url = '/password_change_done/'),name='change_password'),
     path("password_change_done/",PasswordChangeDoneView.as_view(template_name='password_operations/password_change_done.html'),name='password_change_done'),
-    path('search/',search,name='search'),
-    path('activate/<uidb64>/<token>/',activate,name='activate'),
+    #reset password url
+    path("password_reset/",Email_message.as_view(template_name='password_operations/password_reset.html'),name='password_reset'),
+    path("password_reset/done/",PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name='password_reset_done'),
+    path("password_reset_confirm/<uidb64>/<token>/",PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),
+    path("password_reset_complete/",PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),
+    
 ]
